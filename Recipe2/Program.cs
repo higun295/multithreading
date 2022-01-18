ee540f11a3e06a562418f9f54b398a8fc162b606
HEEKWON <higun295@gmail.com>
2022-01-18 11:46:30 +0900

    20220118_1

---
+++ Recipe2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Recipe2 {
+    class Program {
+        static void Main(string[] args) {
+            Thread t = new Thread(PrintNumbersWithDelay);
+            t.Start();
+            PrintNumbers();
+        }
+        static void PrintNumbers() {
+            WriteLine("1 Starting...");
+            for(int i = 1; i < 10; i++) {
+                WriteLine(i);
+            }
+        }
+        static void PrintNumbersWithDelay() {
+            WriteLine("2 Starting...");
+            for(int i = 1; i < 10; i++) {
+                Sleep(TimeSpan.FromSeconds(2));
+                WriteLine(i);
+            }
+        }
+    }
+}
