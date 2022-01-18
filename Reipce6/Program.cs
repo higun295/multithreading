d112ae1faf24ce6f2bed42b57de1aca96bb08d64
HEEKWON <higun295@gmail.com>
2022-01-18 16:48:59 +0900

    20220118_2

---
+++ Reipce6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+using static System.Diagnostics.Process;
+
+namespace Reipce6 {
+    class Program {
+        static void Main(string[] args) {
+            WriteLine($"Current thread priority : {CurrentThread.Priority}");
+            WriteLine("Running on all cores available");
+            RunThreads();
+            Sleep(TimeSpan.FromSeconds(2));
+            WriteLine("Running on a single core");
+            GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
+            RunThreads();
+        }
+
+        static void RunThreads() {
+            var sample = new ThreadSample();
+
+            var threadOne = new Thread(sample.CountNumbers);
+            threadOne.Name = "ThreadOne";
+            var threadTwo = new Thread(sample.CountNumbers);
+            threadTwo.Name = "ThreadTwo";
+
+            threadOne.Priority = ThreadPriority.Highest;
+            threadTwo.Priority = ThreadPriority.Lowest;
+            threadOne.Start();
+            threadTwo.Start();
+        }
+    }
+
+    class ThreadSample {
+        private bool _isStopped = false;
+
+        public void Stop() {
+            _isStopped = true;
+        }
+        public void CountNumbers() {
+            long counter = 0;
+
+            while(!_isStopped) {
+                counter++;
+            }
+
+            WriteLine($"{CurrentThread.Name} with {CurrentThread.Priority,11} priority has a count = {counter,13:NO}");
+        }
+    }
+}
