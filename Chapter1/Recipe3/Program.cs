ab77bdc509413946b846dd41b1cfcddd3e4c3b17
HEEKWON <higun295@gmail.com>
2022-01-24 15:05:03 +0900

    20220124_1

---
+++ Chapter1/Recipe3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Chapter1.Recipe3 {
+    class Program {
+        static void Main(string[] args) {
+            WriteLine("1 Starting...");
+            Thread t = new Thread(PrintNumbersWithDelay);
+            t.Start();
+            t.Join();
+            WriteLine("Thread completed");
+        }
+
+        static void PrintNumbersWithDelay() {
+            WriteLine("2 Starting...");
+            for(int i = 1; i < 10; i++) {
+                Sleep(TimeSpan.FromSeconds(2));
+                WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Chapter2/Recipe2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace Recipe2 {
+    class Program {
+        static void Main(string[] args) {
+            const string MutexName = "CSharpThreadingCookbook";
+
+            using(var m = new Mutex(false, MutexName)) {
+
+            }
+        }
+    }
+}
