3c75978455157f1d4f68f10fb5d54bdb18fe7182
HEEKWON <higun295@gmail.com>
2022-01-27 12:01:40 +0900

    20220127_1

---
+++ TestProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestProject {
+    class Program {
+        static void Main(string[] args) {
+            int length = 10;
+            var actions = new Action[length];
+
+            Console.WriteLine($"클로저(closure) 문제 해결");
+
+            for(var index = 0; index < length; index++) {
+                var localIndex = index;
+                actions[index] = () => Console.WriteLine(localIndex);
+            }
+
+            foreach(var item in actions) {
+                item?.Invoke();
+            }
+        }
+    }
+}
