8b82ea8651ef1fdc81b00fc74086879086f2a8c0
HEEKWON <higun295@gmail.com>
2022-02-16 10:23:18 +0900

    20220216_2

---
+++ Chapter4/Recipe9/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System;
+
+namespace Recipe9 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void ButtonSync_Click(object sender, RoutedEventArgs e) {
+            ContentTextBlock.Text = string.Empty;
+            try {
+
+            }
+            catch(Exception ex) {
+
+            }
+        }
+
+        private void ButtonAsync_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void ButtonAsyncOK_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        Task<string> TaskMethod() {
+            return TaskMethod(TaskScheduler.Default);
+        }
+
+        Task<string> TaskMethod(TaskScheduler scheduler) {
+            Task delay = Task.Delay(TimeSpan.FromSeconds(5));
+
+            return delay.ContinueWith(t => {
+                string str =
+                "Task is running on a thread id " +
+                $"{CurrentThread.ManagedThreadId}. Is thread pool thread : " +
+                $"{CurrentThread.IsThreadPoolThread}";
+
+                ContentTextBlock.Text = str;
+                return str;
+            }, scheduler);
+        }
+    }
+}
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,21 +1,35 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestProject {
     class Program {
         static void Main(string[] args) {
-            int length = 10;
-            var actions = new Action[length];
+            Task<int> task = new Task<int>(() => {
+                Console.WriteLine("TEST0");
+                Thread.Sleep(5000);
+                return 1;
+            });
 
-            Console.WriteLine($"클로저(closure) 문제 해결");
+            task.Start();
+            task.ContinueWith((arg) => {
+                Console.WriteLine("TEST1");
+            });
 
-            for(var index = 0; index < length; index++) {
-                var localIndex = index;
-                actions[index] = () => Console.WriteLine(localIndex);
-            }
 
-            foreach(var item in actions) {
-                item?.Invoke();
-            }
+            //int length = 10;
+            //var actions = new Action[length];
+
+            //Console.WriteLine($"클로저(closure) 문제 해결");
+
+            //for(var index = 0; index < length; index++) {
+            //    var localIndex = index;
+            //    actions[index] = () => Console.WriteLine(localIndex);
+            //}
+
+            //foreach(var item in actions) {
+            //    item?.Invoke();
+            //}
         }
     }
 }
