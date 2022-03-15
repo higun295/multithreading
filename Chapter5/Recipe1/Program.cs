269b630bc22a3be69c60476d62a8a548fdc572bc
HEEKWON <higun295@gmail.com>
2022-03-16 01:13:33 +0900

    20220316_2

---
+++ Chapter5/Recipe1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Recipe1 {
+    class Program {
+        static void Main(string[] args) {
+            Task t = AsynchronyWithTPL();
+            t.Wait();
+
+            t = AsynchronyWithAwait();
+            t.Wait();
+        }
+
+        static Task AsynchronyWithTPL() {
+            Task<string> t = GetInfoAsync("Task 1");
+            Task t2 = t.ContinueWith(task => WriteLine(t.Result), TaskContinuationOptions.NotOnFaulted);
+            Task t3 = t.ContinueWith(task => WriteLine(t.Exception.InnerException), TaskContinuationOptions.OnlyOnFaulted);
+
+            return Task.WhenAny(t2, t3);
+        }
+
+        static async Task AsynchronyWithAwait() {
+            try {
+                WriteLine(await GetInfoAsync("Task 2"));
+            }
+            catch(Exception ex) {
+                WriteLine(ex);
+            }
+        }
+
+        static async Task<string> GetInfoAsync(string name) {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            return $"Task {name} is running on a thread id {CurrentThread.ManagedThreadId}." +
+                $"Is thread pool thread : {CurrentThread.IsThreadPoolThread}";
+        }
+    }
+}
---
+++ Chapter5/Recipe4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Recipe4 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Task t = AsynchronousProcessing();
+            t.Wait();
+
+            Console.WriteLine($"{Convert.ToInt32(sw.Elapsed.TotalSeconds)} seconds passed. Task Finished!!");
+            sw.Stop();
+        }
+
+        static async Task AsynchronousProcessing() {
+            Task<string> t1 = GetInfoAsync("Task 1", 3);
+            Task<string> t2 = GetInfoAsync("Task 2", 5);
+
+            string[] results = await Task.WhenAll(t1, t2);
+            foreach(string result in results) {
+                WriteLine(result);
+            }
+        }
+
+        static async Task<string> GetInfoAsync(string name, int seconds) {
+            //await Task.Delay(TimeSpan.FromSeconds(seconds));
+            await Task.Run(() => Sleep(TimeSpan.FromSeconds(seconds)));
+            return
+                $"Task {name} is running on a thread id " +
+                $"{CurrentThread.ManagedThreadId}." +
+                $"Is thread pool thread : {CurrentThread.IsThreadPoolThread}";
+        }
+    }
+}
